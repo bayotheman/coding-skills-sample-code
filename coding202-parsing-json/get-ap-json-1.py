e1d8c67ee503d288aad701f55823eeeb75c997b0
bayotheman <42247456+bayotheman@users.noreply.github.com>
2020-01-10 15:33:02 +0100

    Update get-ap-json-1.py

--- coding202-parsing-json/get-ap-json-1.py
+++ coding202-parsing-json/get-ap-json-1.py
@@ -2,7 +2,9 @@ from urllib.request import Request, urlopen
 
 req = Request('https://devnetapi.cisco.com/sandbox/mse/api/config/v1/maps/info/DevNetCampus/DevNetBuilding/DevNetZone')
 req.add_header('Authorization', 'Basic bGVhcm5pbmc6bGVhcm5pbmc=')
-response = urlopen(req)
-response_string = response.read().decode('utf-8')
-print(response_string)
-response.close()
+try:
+    with urlopen(req) as response:
+        response_string = response.read().decode('utf-8')
+        print(response_string)
+except Exception as e:
+    print(str(e))
